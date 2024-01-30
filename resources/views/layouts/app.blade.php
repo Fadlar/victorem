40b4df6876bfb0f786b38474a8aba59b119236fa
fadlar <fadlanxyz09@gmail.com>
2024-01-30 20:38:11 +0700

    Home banner

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Male_Fashion Template">
-    <meta name="keywords" content="Male_Fashion, unica, creative, html">
+    <meta name="description" content="Victorem">
+    <meta name="keywords" content="T-shirt, hoodie, clothes, custom">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home | Victorem</title>
